6e67f64387b4d85469bb7538a90a554736178429
rogeriopvl <rogeriopvl@gmail.com>
2019-06-12 22:35:12 +0100

    style: format code with prettier

--- test/main.js
+++ test/main.js
@@ -1,119 +1,117 @@
-/*global describe, it*/
-'use strict';
+/* global describe, it */
+'use strict'
 
-var fs = require('fs');
-var path = require('path');
-var should = require('should');
+var fs = require('fs')
+var path = require('path')
+var should = require('should')
 var Vinyl = require('vinyl')
 
-var jsonLintPlugin = require('../');
+var jsonLintPlugin = require('../')
 
-require('mocha');
+require('mocha')
 
+var getFile = function(filePath) {
+  filePath = 'test/' + filePath
 
-var getFile = function (filePath) {
-    filePath = 'test/' + filePath;
+  return new Vinyl({
+    path: filePath,
+    cwd: 'test/',
+    base: path.dirname(filePath),
+    contents: fs.readFileSync(filePath)
+  })
+}
 
-    return new Vinyl({
-        path: filePath,
-        cwd: 'test/',
-        base: path.dirname(filePath),
-        contents: fs.readFileSync(filePath)
-    });
-};
+describe('gulp-jsonlint', function() {
+  it('should pass file through', function(done) {
+    var cbCounter = 0
 
-describe('gulp-jsonlint', function () {
+    var file = getFile('fixtures/valid.json')
 
-    it('should pass file through', function (done) {
-        var cbCounter = 0;
+    var stream = jsonLintPlugin()
 
-        var file = getFile('fixtures/valid.json');
+    stream.on('data', function(f) {
+      should.exist(f)
+      should.exist(f.relative)
+      should.exist(f.contents)
+      f.path.should.equal('test/fixtures/valid.json')
+      f.relative.should.equal('valid.json')
+      ++cbCounter
+    })
 
-        var stream = jsonLintPlugin();
+    stream.once('end', function() {
+      cbCounter.should.equal(1)
+      done()
+    })
 
-        stream.on('data', function (f) {
-            should.exist(f);
-            should.exist(f.relative);
-            should.exist(f.contents);
-            f.path.should.equal('test/fixtures/valid.json');
-            f.relative.should.equal('valid.json');
-            ++cbCounter;
-        });
+    stream.write(file)
+    stream.end()
+  })
 
-        stream.once('end', function () {
-            cbCounter.should.equal(1);
-            done();
-        });
+  it('should send success status when json is valid', function(done) {
+    var cbCounter = 0
 
-        stream.write(file);
-        stream.end();
-    });
+    var file = getFile('fixtures/valid.json')
 
-    it('should send success status when json is valid', function (done) {
-        var cbCounter = 0;
+    var stream = jsonLintPlugin()
 
-        var file = getFile('fixtures/valid.json');
+    stream.on('data', function(f) {
+      ++cbCounter
+      should.exist(f.jsonlint.success)
+      f.jsonlint.success.should.equal(true)
+      should.not.exist(f.jsonlint.message)
+    })
 
-        var stream = jsonLintPlugin();
+    stream.once('end', function() {
+      cbCounter.should.equal(1)
+      done()
+    })
 
-        stream.on('data', function (f) {
-            ++cbCounter;
-            should.exist(f.jsonlint.success);
-            f.jsonlint.success.should.equal(true);
-            should.not.exist(f.jsonlint.message);
-        });
+    stream.write(file)
+    stream.end()
+  })
 
-        stream.once('end', function () {
-            cbCounter.should.equal(1);
-            done();
-        });
+  it('should send jsonlint error message when json is invalid', function(done) {
+    var cbCounter = 0
 
-        stream.write(file);
-        stream.end();
-    });
+    var file = getFile('fixtures/invalid.json')
 
-    it('should send jsonlint error message when json is invalid', function (done) {
-        var cbCounter = 0;
+    var stream = jsonLintPlugin()
 
-        var file = getFile('fixtures/invalid.json');
+    stream.on('data', function(f) {
+      ++cbCounter
+      should.exist(f.jsonlint.success)
+      f.jsonlint.success.should.equal(false)
+      should.exist(f.jsonlint.message)
+      f.jsonlint.message.should.match(/^Parse error/)
+    })
 
-        var stream = jsonLintPlugin();
+    stream.once('end', function() {
+      cbCounter.should.equal(1)
+      done()
+    })
 
-        stream.on('data', function (f) {
-            ++cbCounter;
-            should.exist(f.jsonlint.success);
-            f.jsonlint.success.should.equal(false);
-            should.exist(f.jsonlint.message);
-            f.jsonlint.message.should.match(/^Parse error/);
-        });
+    stream.write(file)
+    stream.end()
+  })
 
-        stream.once('end', function () {
-            cbCounter.should.equal(1);
-            done();
-        });
+  it('should lint more than one file', function(done) {
+    var cbCounter = 0
+    var file1 = getFile('fixtures/invalid.json')
+    var file2 = getFile('fixtures/valid.json')
 
-        stream.write(file);
-        stream.end();
-    });
+    var stream = jsonLintPlugin()
 
-    it('should lint more than one file', function (done) {
-        var cbCounter = 0;
-        var file1 = getFile('fixtures/invalid.json');
-        var file2 = getFile('fixtures/valid.json');
+    stream.on('data', function() {
+      ++cbCounter
+    })
 
-        var stream = jsonLintPlugin();
+    stream.once('end', function() {
+      cbCounter.should.equal(2)
+      done()
+    })
 
-        stream.on('data', function () {
-            ++cbCounter;
-        });
-
-        stream.once('end', function () {
-            cbCounter.should.equal(2);
-            done();
-        });
-
-        stream.write(file1);
-        stream.write(file2);
-        stream.end();
-    });
-});
+    stream.write(file1)
+    stream.write(file2)
+    stream.end()
+  })
+})
